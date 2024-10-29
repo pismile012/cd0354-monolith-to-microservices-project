bd91bc0d2e92e94c8bb54f72c7b1653d1cfbacf7
duyhoang5891 <hoang.phamduy5891@gmail.com>
2024-10-29 14:06:15 +0700

    spliting backend-api

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=Anh01884585849
+export POSTGRES_HOST=postgres.ccc4nst3pjzt.us-west-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=pj3-202577460078-dev
+export AWS_REGION=us-west-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
